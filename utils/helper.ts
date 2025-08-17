0f430e6cd65b842e62264afa393fe607d6fb2417
Parth Sharma <05sharmaparth@gmail.com>
2025-08-17 14:20:44 +0530

    some url fixes in helper.ts

--- utils/helper.ts
+++ utils/helper.ts
@@ -1,3 +1,5 @@
 export const isDev = process.env.NODE_ENV === 'development';
 
-export const ORIGIN_URL= isDev ? 'http://localhost:3000' : ''
+export const ORIGIN_URL = isDev
+  ? 'http://localhost:3000'
+  : 'https://gistly.vercel.app'; 
