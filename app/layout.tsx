e85f66b0e2c020590392c89e52f70e95cad4147a
Parth Sharma <05sharmaparth@gmail.com>
2025-08-17 14:09:15 +0530

    changes some metadata to deploy

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import Header from '@/components/common/header';
 import Footer from '@/components/common/footer';
 import { ClerkProvider } from '@clerk/nextjs';
 import { Toaster } from 'sonner';
+import { ORIGIN_URL } from '@/utils/helper';
 
 const fontSans = FontSans({
   variable: '--font-sans',
@@ -16,6 +17,18 @@ export const metadata: Metadata = {
   title: 'Gistly - AI-Powered PDF summarization',
   description:
     'Save hours of reading time. Transform lengthy PDFs into clear , accurate summaries in seconds with our advanced AI technology.',
+    openGraph:{
+      images:[
+       {
+         url:'/opengraph-image.png',
+
+       }
+      ]
+    },
+    metadataBase: new URL(ORIGIN_URL),
+    alternates:{
+      canonical:ORIGIN_URL ,
+    }
 };
 
 export default function RootLayout({
